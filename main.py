61d5b65fbc5db35a7a65e06c5739a6020dbb8a2c
Fred Sauer <fredsa@google.com>
2011-11-21 15:18:50 -0800

    refactor and fix

--- main.py
+++ main.py
@@ -104,8 +104,140 @@ class DownloadHandler(blobstore_handlers.BlobstoreDownloadHandler):
 
 
 class MainHandler(webapp.RequestHandler):
-    def get(self):
 
+    def show_latency(self, precision_ms, latency, resource, name, comment):
+        self.response.out.write("""<h1>Latency - %s</h1>""" % name)
+        self.response.out.write("""<div class='comment'>%s</div>""" % comment)
+        if len(latency) == 0:
+          self.response.out.write('No logs')
+          return
+
+        scale = min(1, float(MAX_LATENCY_WIDTH) / max(latency.values()))
+        self.response.out.write("""<pre>""")
+        for k in range(0, max(latency) + 1 ):
+          cnt = latency.get(k, 0)
+          self.response.out.write('%6d requests: [%5d - %5d ms]: %s<br>' % (cnt, k * precision_ms, (k+1) * precision_ms -1, '*' * int(scale * cnt)) )
+        self.response.out.write('</pre>')
+
+        self.response.out.write("""<pre>""")
+        for res in sorted(resource, key=resource.get, reverse=True):
+          self.response.out.write("""%6d requests: %s<br>""" % (resource[res], res) )
+        self.response.out.write("""</pre>""")
+
+
+    def do_mapreduce(self, start_time_usec, end_time_usec):
+        pipeline = MyPipeline(start_time_usec, end_time_usec)
+        logging.info('************************************************************************************************************************************************')
+        logging.info('*************************************************************** pipeline.start() ***************************************************************')
+        logging.info('************************************************************************************************************************************************')
+        pipeline.start()
+
+
+    def do_grep(self, version, max_requests, level, start_time_usec, end_time_usec, precision_ms, raw_logs):
+
+        # --------------- Raw logs ---------------
+        if raw_logs:
+          self.response.out.write("""<h1>Raw logs</h1>""")
+ 
+        latency_errors={}
+        latency_cached={}
+        latency_static={}
+        latency_dynamic={}
+        latency_pending={}
+
+        resource_errors={}
+        resource_cached={}
+        resource_static={}
+        resource_dynamic={}
+        resource_pending={}
+
+        self.response.out.write('%s, %s, %s, %s, %s, %s, %s' % (version, max_requests, level, start_time_usec, end_time_usec, precision_ms, raw_logs) )
+        messages={}
+        count = 0
+        logging.info("fetch(start_time_usec=%s, end_time_usec=%s, ...)" % (start_time_usec, end_time_usec) )
+        for log in logservice.fetch(start_time_usec=start_time_usec,
+                                    end_time_usec=end_time_usec,
+                                    min_log_level=level,
+                                    include_app_logs=True,
+                                    #include_incomplete=True,
+                                    version_ids=[version]
+                                   ):
+          #self.response.out.write('%s<br>' % log)
+
+          # --------------- Raw logs ---------------
+          if raw_logs:
+            data = pprint.pformat(vars(log))
+            data = cgi.escape(data)
+            self.response.out.write('<hr><pre>%s</pre><br>' % data)
+
+          for line in log.line_list():
+            msg = "[%s] %s]" % ( LEVEL[line.level()], line.log_message() )
+            #self.response.out.write('[%s][%s] %s<br>' % (time.strftime('%Y-%m-%d %H:%M:%S %Z', time.localtime(line.time()/1000000)), line.level(), cgi.escape(str( msg ))) )
+            messages[msg] = messages.get(msg, 0) + 1
+            # --------------- Raw logs ---------------
+            if raw_logs:
+              data = pprint.pformat(vars(line))
+              data = cgi.escape(data)
+              self.response.out.write('<pre>%s</pre><br>' % data)
+
+
+
+          res = """[%s] %s""" % (log.status(), log.resource())
+          index = int( (log.latency() - log.pending_time()) / 1000 / precision_ms) 
+          if log.status() >= 400:
+            latency_errors[index] = latency_errors.get(index, 0) + 1
+            resource_errors[res] = resource_errors.get(res, 0) + 1
+          elif log.response_size() == 0:
+            latency_static[index] = latency_static.get(index, 0) + 1
+            resource_static[res] = resource_static.get(res, 0) + 1
+          elif log.status() == 204:
+            latency_cached[index] = latency_cached.get(index, 0) + 1
+            resource_cached[res] = resource_cached.get(res, 0) + 1
+          else:
+            latency_dynamic[index] = latency_dynamic.get(index, 0) + 1
+            resource_dynamic[res] = resource_dynamic.get(res, 0) + 1
+
+          if log.pending_time() > 0:
+            idx = int( log.pending_time() / 1000 / precision_ms)
+            latency_pending[idx] = latency_pending.get(idx, 0) + 1
+            resource_pending[res] = resource_pending.get(res, 0) + 1
+
+
+          count += 1
+          if count == max_requests:
+            break
+
+        if raw_logs:
+          self.response.out.write('<hr>')
+
+        if count == 0:
+          self.response.out.write("""
+            No logs.
+            """)
+          return
+
+        # --------------- Latency ---------------
+        self.show_latency(precision_ms, latency_dynamic, resource_dynamic, 'Dynamic Requests', 'log.response_size() > 0 and log.status() != 204 and log.status() <= 399')
+        self.show_latency(precision_ms, latency_errors,  resource_errors,  'Errors',           'log.status() >= 400')
+        self.show_latency(precision_ms, latency_static,  resource_static,  'Static Requests',  'log.response_size() == 0 and log.status() <= 399')
+        self.show_latency(precision_ms, latency_cached,  resource_cached,  'Cached Requests',  'log.status() == 204')
+        self.show_latency(precision_ms, latency_pending, resource_pending, 'Pending Time',     'log.pending_time() > 0')
+
+        # --------------- Errors ---------------
+        self.response.out.write("""<h1>Log message frequency</h1>""")
+        if len(messages) == 0:
+          self.response.out.write("""
+            No messages.
+            """)
+        else:
+          for msg in sorted(messages, key=messages.get, reverse=True):
+            self.response.out.write("""
+              Count: <b>%d</b><br>
+              <pre class='errmsg'>%s</pre><br>
+              """ % (messages[msg], cgi.escape(msg)) )
+
+
+    def get(self):
         # version
         version = self.request.get('version')
         if not version:
@@ -129,6 +261,7 @@ class MainHandler(webapp.RequestHandler):
         except ValueError:
           precision_ms = 100
 
+        # raw_logs
         raw_logs = self.request.get('raw_logs')
 
         # start_time_usec
@@ -147,6 +280,8 @@ class MainHandler(webapp.RequestHandler):
         except ValueError:
           end_time_usec = time.time() * 1e6
 
+        # desired_action
+        desired_action=self.request.get("desired_action")
 
         #logging.debug("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%DEBUG")
         #logging.info("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%INFO")
@@ -159,7 +294,7 @@ class MainHandler(webapp.RequestHandler):
         self.response.out.write(r"""
           <html>
             <head>
-              <title>Logservice %s %s</title>
+              <title>Logservice %s</title>
               <style>
                 BODY {
                   font-family: arial;
@@ -241,8 +376,12 @@ class MainHandler(webapp.RequestHandler):
 
             </head>
             <body>
-          """ % (app_identity.get_application_id(), version) )
+          """ % app_identity.get_application_id() )
 
+        # --------------- TITLE ---------------
+        self.response.out.write("""
+            <div><a href="/">&lt;&lt; logservice</a></div>
+          """)
         # --------------- 1st FORM ---------------
         self.response.out.write("""
           <fieldset style='background-color: #def'>
@@ -284,6 +423,7 @@ class MainHandler(webapp.RequestHandler):
         self.response.out.write("""
               <input type='checkbox' name='raw_logs' %s> Pretty print raw logs<br>
 
+              <input type='hidden' name='desired_action' value='grep'>
               <input type='submit' value='Summarize'>
 
             </form>
@@ -304,17 +444,18 @@ class MainHandler(webapp.RequestHandler):
  
  
         self.response.out.write("""
+              <input type='hidden' name='desired_action' value='mapreduce'>
               <input type='submit' value='Kick Off MapReduce'>
 
             </form>
           </fieldset>
-          <br>""")
+          """)
 
 
         # --------------- MapReduce results ---------------
         self.response.out.write("""<h1>MapReduce results</h1>""")
         self.response.out.write("""
-          <div id="visualization" style="width: 500px; height: 400px;">visualization</div>
+          <div id="visualization" style="width: 500px; height: 400px; border: 1px solid gray;">visualization</div>
           <hr>
           <pre id="csv-data">csv-data</pre>
           <hr>
@@ -332,140 +473,18 @@ class MainHandler(webapp.RequestHandler):
         start_time_usec = (now_s - 1 * 60 * 60) * 1e6
         end_time_usec = now_s * 1e6
 
-        pipeline = MyPipeline(start_time_usec, end_time_usec)
-        logging.info('************************************************************************************************************************************************')
-        logging.info('*************************************************************** pipeline.start() ***************************************************************')
-        logging.info('************************************************************************************************************************************************')
-        pipeline.start()
-
-
-
-        # --------------- Raw logs ---------------
-        if raw_logs:
-          self.response.out.write("""<h1>Raw logs</h1>""")
-
-        latency_errors={}
-        latency_cached={}
-        latency_static={}
-        latency_dynamic={}
-        latency_pending={}
-
-        resource_errors={}
-        resource_cached={}
-        resource_static={}
-        resource_dynamic={}
-        resource_pending={}
-
-        messages={}
-        count = 0
-        logging.info("fetch(start_time_usec=%s, end_time_usec=%s, ...)" % (start_time_usec, end_time_usec) )
-        for log in logservice.fetch(start_time_usec=start_time_usec,
-                                    end_time_usec=end_time_usec,
-                                    min_log_level=level,
-                                    include_app_logs=True,
-                                    #include_incomplete=True,
-                                    version_ids=[version]
-                                   ):
-          #self.response.out.write('%s<br>' % log)
-
-          # --------------- Raw logs ---------------
-          if raw_logs:
-            data = pprint.pformat(vars(log))
-            data = cgi.escape(data)
-            self.response.out.write('<hr><pre>%s</pre><br>' % data)
-
-          for line in log.line_list():
-            msg = "[%s] %s]" % ( LEVEL[line.level()], line.log_message() )
-            #self.response.out.write('[%s][%s] %s<br>' % (time.strftime('%Y-%m-%d %H:%M:%S %Z', time.localtime(line.time()/1000000)), line.level(), cgi.escape(str( msg ))) )
-            messages[msg] = messages.get(msg, 0) + 1
-            # --------------- Raw logs ---------------
-            if raw_logs:
-              data = pprint.pformat(vars(line))
-              data = cgi.escape(data)
-              self.response.out.write('<pre>%s</pre><br>' % data)
-
-
-
-          res = """[%s] %s""" % (log.status(), log.resource())
-          index = int( (log.latency() - log.pending_time()) / 1000 / precision_ms) 
-          if log.status() >= 400:
-            latency_errors[index] = latency_errors.get(index, 0) + 1
-            resource_errors[res] = resource_errors.get(res, 0) + 1
-          elif log.response_size() == 0:
-            latency_static[index] = latency_static.get(index, 0) + 1
-            resource_static[res] = resource_static.get(res, 0) + 1
-          elif log.status() == 204:
-            latency_cached[index] = latency_cached.get(index, 0) + 1
-            resource_cached[res] = resource_cached.get(res, 0) + 1
-          else:
-            latency_dynamic[index] = latency_dynamic.get(index, 0) + 1
-            resource_dynamic[res] = resource_dynamic.get(res, 0) + 1
-
-          if log.pending_time() > 0:
-            idx = int( log.pending_time() / 1000 / precision_ms)
-            latency_pending[idx] = latency_pending.get(idx, 0) + 1
-            resource_pending[res] = resource_pending.get(res, 0) + 1
-
-
-          count += 1
-          if count == max_requests:
-            break
-
-        if raw_logs:
-          self.response.out.write('<hr>')
-
-        if count == 0:
-          self.response.out.write("""
-            No logs.
-            """)
-          return
-
-
-        # --------------- Latency ---------------
-        def show_latency(latency, resource, name, comment):
-          self.response.out.write("""<h1>Latency - %s</h1>""" % name)
-          self.response.out.write("""<div class='comment'>%s</div>""" % comment)
-          if len(latency) == 0:
-            self.response.out.write('No logs')
-            return
-
-          scale = min(1, float(MAX_LATENCY_WIDTH) / max(latency.values()))
-          self.response.out.write("""<pre>""")
-          for k in range(0, max(latency) + 1 ):
-            cnt = latency.get(k, 0)
-            self.response.out.write('%6d requests: [%5d - %5d ms]: %s<br>' % (cnt, k * precision_ms, (k+1) * precision_ms -1, '*' * int(scale * cnt)) )
-          self.response.out.write('</pre>')
-
-          self.response.out.write("""<pre>""")
-          for res in sorted(resource, key=resource.get, reverse=True):
-            self.response.out.write("""%6d requests: %s<br>""" % (resource[res], res) )
-          self.response.out.write("""</pre>""")
-
-
-        show_latency(latency_dynamic, resource_dynamic, 'Dynamic Requests', 'log.response_size() > 0 and log.status() != 204 and log.status() <= 399')
-        show_latency(latency_errors,  resource_errors,  'Errors',           'log.status() >= 400')
-        show_latency(latency_static,  resource_static,  'Static Requests',  'log.response_size() == 0 and log.status() <= 399')
-        show_latency(latency_cached,  resource_cached,  'Cached Requests',  'log.status() == 204')
-        show_latency(latency_pending, resource_pending, 'Pending Time',     'log.pending_time() > 0')
-
-        # --------------- Errors ---------------
-        self.response.out.write("""<h1>Log message frequency</h1>""")
-        if len(messages) == 0:
-          self.response.out.write("""
-            No messages.
-            """)
-        else:
-          for msg in sorted(messages, key=messages.get, reverse=True):
-            self.response.out.write("""
-              Count: <b>%d</b><br>
-              <pre class='errmsg'>%s</pre><br>
-              """ % (messages[msg], cgi.escape(msg)) )
+        if desired_action == "mapreduce":
+          self.do_mapreduce(start_time_usec, end_time_usec)
+        elif desired_action == "grep":
+          self.do_grep(version, max_requests, level, start_time_usec, end_time_usec, precision_ms, raw_logs)
 
         # --------------- End of page ---------------
         self.response.out.write("""
             </body>
           </html>
           """)
+
+
 
 APP = webapp.WSGIApplication(
     [
